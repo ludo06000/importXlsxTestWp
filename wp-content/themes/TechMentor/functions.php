5b463a388992e54d51d91881122839f2a99fef7b
ludo06000 <lc06000@gmail.com>
2022-06-17 14:35:35 +0200

    commit

--- wp-content/themes/TechMentor/functions.php
+++ wp-content/themes/TechMentor/functions.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace App;
-
 function mytheme_supports(){
     add_theme_support('title-tag');
 };
@@ -11,30 +9,7 @@ function mytheme_register_assets() {
     wp_enqueue_style( 'bootstrap' );
 
 };
-
-function mentormarketing_product() {
-
-    // CPT Product 
-    $labels = array(
-        'name' => 'mentormarketing_product',
-        'add_new_item' => 'Ajouter un fichier',
-        'edit_new-item' => 'Modifier un produit',
-        'menu-name' => 'Produits',
-    );
-
-    $args = array(
-        'labels' => $labels,
-        'public' => true,
-        'menu_position' => 3,
-        'menu_icon' => 'dashicons-products',
-    );
-
-    register_post_type( 'Produits', $args);
-}
   
-
-
-add_action('init', 'App\mentormarketing_product'); // Le hook init lance la fonction
 add_action('after_setup_theme', 'App\mytheme_supports');
 add_action('wp_enqueue_scripts','App\mytheme_register_assets');
 
